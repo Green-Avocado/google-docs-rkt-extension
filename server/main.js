d1318fdbd1073e45da9a571f13bec2d0fd73383e
Green-Avocado <greenavocado@protonmail.com>
2020-11-13 05:00:50 -0800

    feat: added handling for errors and SIGINT

--- server/main.js
+++ server/main.js
@@ -10,37 +10,69 @@ const port = process.env.PORT;
 const { v4: uuidv4 } = require('uuid');
 
 const server = http.createServer((req, res) => {
-	let body = [];
-	req.on('data', (chunk) => {
-		body.push(chunk);
-	}).on('end', () => {
-		body = Buffer.concat(body).toString();
-		let data = JSON.parse(body);
-        let filename = uuidv4() + '.rkt~';
+    try {
+        let body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            let data = JSON.parse(body);
+            let filename = uuidv4() + '.rkt~';
 
-		fs.writeFile(filename, data.content, function (err) {
-			if (err) throw err;
+            fs.writeFile(filename, data.content, function (err) {
+                if (err) throw err;
 
-			console.log('Saved!');
+                console.log(`Saved to ${filename}`);
 
-			exec(`raco test ${filename}`, (err, stdout, stderr) => {
-				if (err) {
-				  throw err;
-				}
+                exec(`raco test ${filename}`, (err, stdout, stderr) => {
+                    if (err) {
+                        console.log(err);
 
-				console.log(`stdout: ${stdout}`);
-				
-				res.statusCode = 200;
-				res.setHeader('Content-Type', 'text/plain');
-				res.end(stdout);
-			});
+                        res.statusCode = 500;
+                        res.end(JSON.stringify({
+                            status: `Error: ${err}`,
+                            output: null
+                        }));
+                    }
 
-            fs.unlinkSync(filename)
-		});
-	});
+                    console.log(`stdout: ${stdout}`);
+
+                    fs.unlinkSync(filename)
+                    
+                    res.statusCode = 200;
+                    res.end(JSON.stringify({
+                        status: "Success",
+                        output: stdout
+                    }));
+                });
+            });
+        });
+    }
+    catch(err) {
+        console.log(err);
+
+        res.statusCode = 500;
+        res.end(JSON.stringify({
+            status: `Error: ${err}`,
+            output: null
+        }));
+    }
 });
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+process.on('SIGINT', function() {
+    console.log('\nGracefully shutting down from SIGINT (Ctrl-C)\n');
+
+    console.log('Removing .rkt~ files... ');
+
+    exec('rm -f *.rkt~', (err, stdout, stderr) => {
+        if(err) console.log(err);
+
+        console.log('done\n');
+        process.exit();
+    });
 });
 
