0594b0d0e66a3ec16ede5f815d0348697bc8e21a
Green-Avocado <greenavocado@protonmail.com>
2020-11-23 11:17:58 -0800

    feat: updated server to pass compile requests to local server

--- web-app/index.js
+++ web-app/index.js
@@ -1,8 +1,11 @@
 #!/bin/node
+var proxy = require('express-http-proxy');
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.use('/rkt-help', express.static('public'));
+app.use('/compile', proxy('localhost:8000'));
+app.use('*', (req, res) => res.sendStatus(404));
 
-app.listen(8001);
+app.listen(8443);
 
